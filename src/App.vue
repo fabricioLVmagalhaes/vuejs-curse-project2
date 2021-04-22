a236a71bf9b8b986e790438f1220b375c67dd221
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-22 20:05:02 -0300

    Setup & First Steps

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation',
+          link: 'http://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'http://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
